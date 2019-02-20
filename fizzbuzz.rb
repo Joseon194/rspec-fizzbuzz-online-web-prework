e85a6ddfe634fbbec3cd535d39d403a3f99a9166
Dennis Chung <chungdennis6@gmail.com>
2019-02-20 02:04:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if (int % 3 == 0) && (int % 5 != 0)
+  if int % 3 == 0 && int % 5 != 0
     "Fizz"
-elsif (int % 5 == 0) && (int % 3 != 0)
+elsif int % 3 != 0
     "Buzz"
-elsif (int % 3 == 0) && (int % 5 == 0)
+elsif int % 5 == 0
     "FizzBuzz"
 else
-  nil
+  "Buzz"
 end
 end
